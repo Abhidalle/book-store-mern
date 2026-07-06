b9c1ef178116160269dd6ed75be7a22f5614bef4
Abhidalle <abhiadhikari2009@gmail.com>
2026-07-06 20:18:06 +0545

    map backend book data to frontend table

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -24,9 +24,8 @@ const Home = () => {
                 setLoading(false);
             });
     }, []);
-
     return (
-        <div className="p-4">
+<div className="p-4">
             <div className="flex justify-between items-center">
                 <h1 className="text-3xl my-8">Books List</h1>
                 <Link to="/books/create">
@@ -34,12 +33,60 @@ const Home = () => {
                 </Link>
             </div>
             
-            {loading ? (
+            {loading ? ( 
                 <Spinner />
             ) : (
                 <div className="mt-4">
-                
-                    <p>Books loaded successful ly</p> 
+                    <table className="w-full border-separate border-spacing-2">
+                        <thead>
+                            <tr>
+                                <th className="border border-slate-600 rounded-md">No</th>
+                                <th className="border border-slate-600 rounded-md">Title</th>
+                                <th className="border border-slate-600 rounded-md max-md:hidden">Author</th>
+                                <th className="border border-slate-600 rounded-md max-md:hidden">Publish Year</th>
+                                <th className="border border-slate-600 rounded-md">Operations</th>
+                            </tr>
+                        </thead>
+
+                        <tbody>
+                            {books.map((book, index) => (
+                                <tr key={book._id} className="h-8">
+                                    <td className="border border-slate-700 rounded-md text-center">
+                                        {index + 1}
+                                    </td>
+                                    <td className="border border-slate-700 rounded-md text-center">
+                                        {book.title}
+                                    </td>
+                                    <td className="border border-slate-700 rounded-md text-center max-md:hidden">
+                                        {book.author}
+                                    </td>
+                                    <td className="border border-slate-700 rounded-md text-center max-md:hidden">
+                                        {book.publishYear}
+                                    </td>
+                                    <td className="border border-slate-700 rounded-md text-center">
+                                        <div className="flex justify-center gap-x-4">
+                                            
+                                            {/* View Details Icon */}
+                                            <Link to={`/books/details/${book._id}`}>
+                                                <BsInfoCircle className="text-2xl text-green-800" />
+                                            </Link>
+                                            
+                                            {/* Edit Icon */}
+                                            <Link to={`/books/edit/${book._id}`}>
+                                                <AiOutlineEdit className="text-2xl text-yellow-600" />
+                                            </Link>
+
+                                            {/* Delete Icon */}
+                                            <Link to={`/books/delete/${book._id}`}>
+                                                <MdOutlineDelete className="text-2xl text-red-600" />
+                                            </Link>
+
+                                        </div>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table> 
                 </div>
             )}
         </div>
